dd9b7b8e76c0f9602e34abc04658c17bcd7fcf2a
Minahil Mirza <minahilmirza948@email.com>
2026-04-26 17:36:53 +0500

    loading dataset, clean it, print requirements

--- task1.py
+++ task1.py
@@ -1,2 +1,10 @@
 import pandas as pd
 
+dataset=pd.read_csv("Iris.csv") 
+dfcleaned=dataset.dropna() 
+sh=dfcleaned.shape 
+print("shape is :", sh)
+print("column names :",dfcleaned.columns) 
+print(dfcleaned.head(10))
+print(dfcleaned.info())
+print(dfcleaned.describe())

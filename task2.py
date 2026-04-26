91fd074e4d1b5ae00b83da0ef8a8aa0a9c2f8772
Minahil Mirza <minahilmirza948@email.com>
2026-04-27 00:31:31 +0500

    plotting graph for visualization

--- task2.py
+++ task2.py
@@ -1,38 +1,41 @@
 import yfinance as yf
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+data = yf.download("AAPL", start="2015-01-01", end="2024-01-01", auto_adjust=True)
 
-data=yf.download("AAPL", start="2015-01-01", end="2024-01-01",
-                 auto_adjust=True,interval="1d")
-print(data.head(10))
-
-
-data["target"]=data["Close"].shift(-1)
+data["target"] = data["Close"].shift(-1)
 data = data.dropna()
 
-x=data[["High","Low","Open","Volume"]]
-y=data["target"]
-X_train, X_test, Y_train ,Y_test=train_test_split(x,y,test_size=0.2)
+X = data[["High", "Low", "Open", "Volume"]]
+y = data["target"]
 
-split=int(len(data)*0.8)
+split = int(len(data) * 0.8)
 
-X_train=x[:split]
-X_test=x[split:]
+X_train, X_test = X[:split], X[split:]
+y_train, y_test = y[:split], y[split:]
 
-Y_train=y[:split]
-Y_test=y[split:]
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("MSE:", mean_squared_error(y_test, y_pred))
+print("R²:", r2_score(y_test, y_pred))
+
+# plotting
+plt.figure(figsize=(8,5))
+
+sns.scatterplot(x=y_test, y=y_pred)
+plt.xlabel("Actual Closing Price")
+plt.ylabel("Predicted Closing Price")
+plt.title("Actual vs Predicted Closing Prices")
 
 
-model=LinearRegression()
-model.fit(X_train,Y_train)
+plt.plot([y_test.min(), y_test.max()],
+         [y_test.min(), y_test.max()],
+         color="red")
 
-y_pred=model.predict(X_test)
-
-MSE=mean_squared_error(Y_test,y_pred)
-Rsq=r2_score(Y_test,y_pred)
-
-print("mean square error is: ",MSE)
-print("value of R square is: ",Rsq)
-
+plt.show()

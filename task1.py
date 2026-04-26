505d71fafe60282c1c8846cefc8fc776925f714e
Minahil Mirza <minahilmirza948@email.com>
2026-04-26 17:57:04 +0500

    visualizing scatter plot

--- task1.py
+++ task1.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 
 dataset=pd.read_csv("Iris.csv") 
@@ -8,3 +9,32 @@ print("column names :",dfcleaned.columns)
 print(dfcleaned.head(10))
 print(dfcleaned.info())
 print(dfcleaned.describe())
+
+
+fig ,ax =plt.subplots(1,2)
+petallength= dfcleaned['PetalLengthCm']
+petalwidth=dfcleaned['PetalWidthCm']
+ax[0].scatter(
+    petallength,petalwidth, color='green'
+
+)
+ax[0].set_xlabel('petal lenth')
+ax[0].set_ylabel('petal width')
+
+ax[0].set_title('relationship btw petals')
+
+
+
+sepallength= dfcleaned['SepalLengthCm']
+sepallwidth=dfcleaned['SepalWidthCm']
+ax[1].scatter(
+    sepallength,sepallwidth, color='green'
+
+)
+ax[1].set_xlabel('sepal lenth')
+ax[1].set_ylabel('sepal width')
+
+ax[1].set_title('relationship btw sepals')
+
+plt.tight_layout()
+plt.show()
